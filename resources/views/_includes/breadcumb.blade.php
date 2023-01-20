2f3a3093c4f88d1ab2ba2e5128ea575fb58abb9e
ogisetiawan <ogisetiawan21@gmail.com>
2023-01-20 14:59:42 +0700

    style: breadcumb size and segment

--- resources/views/_includes/breadcumb.blade.php
+++ resources/views/_includes/breadcumb.blade.php
@@ -1,3 +1,5 @@
+@php $link = "" @endphp
+
 <!--begin::Toolbar-->
 <div class="toolbar mb-n1 mb-lg-n3" id="kt_toolbar">
     <!--begin::Container-->
@@ -5,23 +7,22 @@
         <!--begin::Page title-->
         <div class="page-title d-flex flex-column align-items-start me-3 py-2 py-lg-0 gap-2">
             <!--begin::Title-->
-            <h1 class="d-flex text-dark fw-bold m-0 fs-3">Users</h1>
+            <h1 class="d-flex text-dark fw-bold m-0 fs-3">{{ ucwords(str_replace('_',' ',Request::segment(3))) }}</h1>
             <!--begin::Breadcrumb-->
-            <ul class="breadcrumb breadcrumb-dot fw-semibold text-gray-600 fs-7">
-                <!--begin::Item-->
-                <li class="breadcrumb-item text-gray-600">
-                    <a href="{{ url('admin') }}" class="text-gray-600 text-hover-primary">Home</a>
-                </li>
-                <!--end::Item-->
-                <!--begin::Item-->
-                <li class="breadcrumb-item text-gray-600">User Management</li>
-                <!--end::Item-->
-                <!--begin::Item-->
-                <li class="breadcrumb-item text-gray-500">Users List</li>
-                <!--end::Item-->
+            <ul class="breadcrumb breadcrumb-dot fw-semibold text-gray-600 fs-6">
+                @for($i = 1; $i <= count(Request::segments()); $i++) 
+                    @if ($i < count(Request::segments()) & $i> 0)
+                        @php $link .= "/" . Request::segment($i); @endphp
+                        <li class="breadcrumb-item text-gray-600">
+                            <a href="{{ $link }}" class="text-gray-600 text-hover-primary">{{ ucwords(str_replace('_',' ',Request::segment($i)))}}</a>&nbsp;
+                        </li>
+                    @else
+                        <li class="breadcrumb-item text-gray-500">{{ucwords(str_replace('_',' ',Request::segment($i)))}}</li>
+                    @endif
+                @endfor
             </ul>
             <!--end::Breadcrumb-->
-            <!--end::Title-->
+                <!--end::Title-->
         </div>
         <!--end::Page title-->
     </div>
